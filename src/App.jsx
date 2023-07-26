8b761f1cc14f83a9ebd8e7ee99bfe94a0b7815c2
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2023-07-26 20:52:48 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react'
+import { getData, getPokemon } from './services/data.jsx'
 import './App.css'
+import Card from './components/Card/index.jsx'
+import Navbar from './components/Navbar/index.jsx';
+
 
 function App() {
   // for actual setting and storing data
@@ -19,17 +23,65 @@ function App() {
       // referencing the pages
       setNextUrl(response.next);
       setPrevUrl(response.previous);
+      console.log(response);
       // loading is set as false bcoz we have retrieved the data
+      await loadingData(response.results);
       setLoading(false);
     }
+    fetchData();
   }, [])
 
+  const next = async () => {
+    setLoading(true);
+    let data = await getData(nextUrl);
+    await loadingData(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const prev = async () => {
+    if(!prevUrl){
+      return ;
+    }
+    setLoading(true);
+    let data = await getData(prevUrl);
+    await loadingData(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  // iterating over all qualities and storing the data
+  const loadingData = async (data) => {
+    let value = await Promise.all(data.map(async value => {
+      let record = await getPokemon(value.url);
+      return record;
+    }))
+    setpokeData(value)
+  }
+
   return (
-    <div className='App'>
-          {loading ? <h1>Loading....</h1> : null}
-          {loading === false && <h1>Data fetched...</h1>}
-    </div>
-  )
+    <>
+      <Navbar />
+      <div>
+        {loading ? <h1 style={{ textAlign: 'center' }}>Loading...</h1> : (
+          <>
+            <div className="grid-container">
+              {pokeData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={prev}>Prev</button>
+              <button onClick={next}>Next</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+  
 }
 
 export default App
---
+++ src/services/data.jsx
@@ -0,0 +1,22 @@
+export async function getData(url){
+    // fetching all the data from the api endpoint
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                resolve(data);
+            })
+    })
+}
+
+
+export async function getPokemon(url){
+    // fetching the individual data of a pokemon
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                resolve(data);
+            })
+    })
+}
